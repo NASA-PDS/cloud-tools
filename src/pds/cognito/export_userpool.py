6b3235fcbc8bf946c4bc09df11ef688fd8ffd27d
jimmie <jimmie.d.young@gmail.com>
2025-04-22 14:11:08 -0700

    Additional fixes reported from black, reorder-imports, etc. hooks

--- src/pds/cognito/export_userpool.py
+++ src/pds/cognito/export_userpool.py
@@ -1,7 +1,8 @@
 """Extract the list of users from the identified Cognito user pool."""
 
-import sys
 import datetime
+import sys
+
 import boto3
 
 
@@ -9,21 +10,18 @@ def get_cognito_csv_header(cognito_client, user_pool_id):
     """Retrieve the csv header of the user pool."""
     result = None
     csv_header_record = cognito_client.get_csv_header(UserPoolId=user_pool_id)
-    if 'CSVHeader' in csv_header_record:
-        result = csv_header_record['CSVHeader']
+    if "CSVHeader" in csv_header_record:
+        result = csv_header_record["CSVHeader"]
 
     return result
 
 
 def get_cognito_users(cognito_client, user_pool_id, next_pagination_token, limit):
     """Get next page of users from the user pool based on the given pagination token."""
-    return cognito_client.list_users(
-        UserPoolId=user_pool_id,
-        Limit=limit,
-        PaginationToken=next_pagination_token
-    ) if next_pagination_token else cognito_client.list_users(
-        UserPoolId=user_pool_id,
-        Limit=limit
+    return (
+        cognito_client.list_users(UserPoolId=user_pool_id, Limit=limit, PaginationToken=next_pagination_token)
+        if next_pagination_token
+        else cognito_client.list_users(UserPoolId=user_pool_id, Limit=limit)
     )
 
 
@@ -31,9 +29,9 @@ def extract_values(header_list, user_record):
     """Extract attributes according to the given header list."""
     # put the Username and Attributes in a form that's easier to search across
     user_dict = {}
-    user_dict['cognito:username'] = user_record['Username']
-    for attr in user_record['Attributes']:
-        user_dict[attr['Name'].lower()] = attr['Value']
+    user_dict["cognito:username"] = user_record["Username"]
+    for attr in user_record["Attributes"]:
+        user_dict[attr["Name"].lower()] = attr["Value"]
 
     result = []
     for header_name in header_list:
@@ -62,22 +60,17 @@ user_pool_id = sys.argv[1]
 region = sys.argv[2]
 page_size = int(sys.argv[3])
 
-cognito_client = boto3.client('cognito-idp', region)
+cognito_client = boto3.client("cognito-idp", region)
 
 csv_header = get_cognito_csv_header(cognito_client, user_pool_id)
-print(','.join(csv_header))
+print(",".join(csv_header))
 
 pagination_token = ""
 while pagination_token is not None:
-    user_records = get_cognito_users(
-        cognito_client,
-        user_pool_id,
-        pagination_token,
-        page_size
-    )
+    user_records = get_cognito_users(cognito_client, user_pool_id, pagination_token, page_size)
 
-    pagination_token = user_records.get('PaginationToken')
+    pagination_token = user_records.get("PaginationToken")
 
-    if 'Users' in user_records:
-        for user in user_records['Users']:
+    if "Users" in user_records:
+        for user in user_records["Users"]:
             print(",".join(extract_values(csv_header, user)))
