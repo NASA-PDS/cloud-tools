90cf405a812758af0e5589f1d4af1ac1b2cf0393
jimmie <jimmie.d.young@gmail.com>
2025-07-09 13:44:01 -0700

    Employ improved version of handling pkg_resources and extend_path

--- src/pds/__init__.py
+++ src/pds/__init__.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
 """PDS Namespace."""
 
-import pkgutil
-__path__ = pkgutil.extend_path(__path__, __name__)
+try:
+    import setuptools
+    version = setuptools.__version__.split('.')
+    if int(version[0]) <= 67 and int(version[1]) < 3:
+        try:
+            __import__('pkg_resources').declare_namespace(__name__)
+        except:
+            import pkgutil
+            __path__ = pkgutil.extend_path(__path__, __name__)
+except ImportError:
+    pass
