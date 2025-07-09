6772e8b4eecb9314be2ac6598f236fa12468d7a1
jimmie <jimmie.d.young@gmail.com>
2025-07-09 13:48:08 -0700

    Will the precheck madness ever end. Fixed flake8 complaint about __init__.py

--- src/pds/__init__.py
+++ src/pds/__init__.py
@@ -7,7 +7,7 @@ try:
     if int(version[0]) <= 67 and int(version[1]) < 3:
         try:
             __import__('pkg_resources').declare_namespace(__name__)
-        except:
+        except Exception:
             import pkgutil
             __path__ = pkgutil.extend_path(__path__, __name__)
 except ImportError:
