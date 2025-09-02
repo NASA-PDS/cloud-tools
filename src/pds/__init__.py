215f488a0d1fc034b762ad72b75bd7743ebe76e3
Sean Kelly <kelly@seankelly.biz>
2025-09-02 11:46:45 -0500

    Upgrade to Python 3.13

--- src/pds/__init__.py
+++ src/pds/__init__.py
@@ -1,16 +1,2 @@
 # -*- coding: utf-8 -*-
 """PDS Namespace."""
-
-try:
-    import setuptools
-
-    version = setuptools.__version__.split(".")
-    if int(version[0]) <= 67 and int(version[1]) < 3:
-        try:
-            __import__("pkg_resources").declare_namespace(__name__)
-        except Exception:
-            import pkgutil
-
-            __path__ = pkgutil.extend_path(__path__, __name__)
-except ImportError:
-    pass
